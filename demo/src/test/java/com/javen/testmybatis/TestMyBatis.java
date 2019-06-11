c93f96dfcc12f701c383a5c95c479c16a2bf496f
Deft <253386581@qq.com>
2019-06-11 17:09:25 +0800

    1.驱动和数据库不匹配  2.JUnit Test

--- demo/src/test/java/com/javen/testmybatis/TestMyBatis.java
+++ demo/src/test/java/com/javen/testmybatis/TestMyBatis.java
@@ -1,5 +1,28 @@
 package com.javen.testmybatis;
 
-public class TestMyBatis {
+import javax.annotation.Resource;
 
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSON;
+import com.javen.model.User;
+import com.javen.service.IUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)//表示继承了SpringJUnit4ClassRunner类  
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
+public class TestMyBatis {
+	private static Logger logger = Logger.getLogger(TestMyBatis.class);
+	
+	@Resource
+	private IUserService userService = null;
+	
+	@Test
+	public void Test() {
+		User user = userService.getUserById(1);
+		logger.info(JSON.toJSONString(user));
+	}
 }
