e7eba28ce713fa2f48a32e6610cea5c3c852509c
Deft <253386581@qq.com>
2019-06-12 16:24:01 +0800

    D:/software/public tools/Git/user/..  各种接口的写法

--- demo/src/main/java/com/javen/controller/UserController.java
+++ demo/src/main/java/com/javen/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.javen.controller;
 
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javen.model.User;
 import com.javen.service.IUserService;
@@ -53,4 +60,46 @@ public class UserController {
         model.addAttribute("user", user);  
         return "showUser";  
 	}
+	
+	 // /user/showUser2?id=1
+    @RequestMapping(value="/showUser2",method=RequestMethod.GET)  
+    public String toIndex2(@RequestParam("id") String id,Model model){  
+        int userId = Integer.parseInt(id);  
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);  
+        log.debug(user.toString());
+        model.addAttribute("user", user);  
+        return "showUser";  
+    } 
+    
+    // /user/showUser3/{id}
+    @RequestMapping(value="/showUser3/{id}",method=RequestMethod.GET)  
+    public String toIndex3(@PathVariable("id")String id,Map<String, Object> model){  
+        int userId = Integer.parseInt(id);  
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);  
+        log.debug(user.toString());
+        model.put("user", user);  
+        return "showUser";  
+    }
+    
+    // /user/{id}
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)  
+    public @ResponseBody User getUserInJson(@PathVariable String id,Map<String, Object> model){  
+        int userId = Integer.parseInt(id);  
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);  
+        log.info(user.toString());
+        return user;  
+    }
+    
+    // /user/jsontype/{id}
+    @RequestMapping(value="/jsontype/{id}",method=RequestMethod.GET)  
+    public ResponseEntity<User>  getUserInJson2(@PathVariable String id,Map<String, Object> model){  
+        int userId = Integer.parseInt(id);  
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);  
+        log.info(user.toString());
+        return new ResponseEntity<User>(user,HttpStatus.OK);  
+    }
 }
